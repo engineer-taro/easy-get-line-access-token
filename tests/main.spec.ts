178b8e5c926095767dbcf150fe885a0e7d61ae66
cm-engineer-taro <tsukamoto.taro@classmethod.jp>
2024-03-01 00:36:44 +0900

    Add LIFF URL, LINE email address, and LINE password to .env.example*** ***Update playwright.config.ts to set locale to 'ja' for chromium project*** ***Add .cspell.json with spelling configuration*** ***Update package.json to use playwright test command and add test:h script*** ***Remove example.spec.ts and add main.spec.ts for testing LINE login

---
+++ tests/main.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
+
+test('アクセストークン取得', async ({ page }) => {
+  checkSettingEnv();
+  await page.goto(process.env.LIFF_URL || '');
+
+  await expect(page).toHaveTitle(/LINE Login/, { timeout: 15000 });
+
+  await page
+    .getByPlaceholder('メールアドレス')
+    .fill(process.env.LINE_MAIL_ADDRESS || '');
+  await page
+    .getByPlaceholder('パスワード')
+    .fill(process.env.LINE_PASSWORD || '');
+
+  await page.click('text=ログイン');
+
+  await page.waitForLoadState('networkidle');
+  const friendRecommendation = await page.getByText(' 許可が必要な項目 ');
+  if ((await friendRecommendation.count()) > 0) {
+    throw new Error(
+      '初回の友達登録画面が表示されているため、手動で許可したのちに再度実行してください'
+    );
+  }
+
+  const context = await page.context().storageState();
+  let accessToken = '';
+  context.origins.forEach((origin) => {
+    origin.localStorage?.forEach((item) => {
+      if (item.name.includes('accessToken')) {
+        accessToken = item.value;
+      }
+    });
+  });
+  console.log(accessToken);
+});
+
+const checkSettingEnv = () => {
+  if (!process.env.LIFF_URL) {
+    throw new Error('環境変数 LIFF_URL が設定されていません');
+  }
+  if (!process.env.LINE_MAIL_ADDRESS) {
+    throw new Error('環境変数 LINE_MAIL_ADDRESS が設定されていません');
+  }
+  if (!process.env.LINE_PASSWORD) {
+    throw new Error('環境変数 LINE_PASSWORD が設定されていません');
+  }
+};
